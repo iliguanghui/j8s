5f94985531c24488168b85320fd60d0bf64de56c
lgypro <kaixin.lgx@gmail.com>
2023-09-14 10:34:21 +0800

    initial commit

---
+++ src/main/java/com/lgypro/j8s/J8sApplication.java
@@ -0,0 +1,11 @@
+package com.lgypro.j8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J8sApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(J8sApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lgypro/j8s/J8sApplicationTests.java
@@ -0,0 +1,12 @@
+package com.lgypro.j8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J8sApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
